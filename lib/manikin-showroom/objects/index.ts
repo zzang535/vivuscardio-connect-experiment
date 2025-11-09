8c0519b94a91be4e0b40ebfcbe77ff2eb78fd5b5
zzang535 <zzang535@gmail.com>
2025-11-09 15:24:54 +0900

    refactor: 조명 상수/유틸 재배치

--- lib/manikin-showroom/objects/index.ts
+++ lib/manikin-showroom/objects/index.ts
@@ -7,8 +7,6 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import * as CONSTANTS from "../constants";
 
-export * from "./light";
-
 /**
  * Canvas를 사용하여 텍스트 텍스처 생성
  * @param title 상단에 표시할 제목 (큰 폰트)
@@ -188,6 +186,51 @@ export function createTable(
   table.receiveShadow = true;
 
   return table;
+}
+
+/**
+ * 조명 설정 생성 및 씬에 추가
+ * @param scene Three.js Scene 객체
+ */
+export function setupLights(scene: THREE.Scene): void {
+  const ambientLight = new THREE.AmbientLight(
+    0xffffff,
+    CONSTANTS.AMBIENT_LIGHT.INTENSITY
+  );
+  scene.add(ambientLight);
+
+  const mainLight = new THREE.DirectionalLight(
+    0xffffff,
+    CONSTANTS.MAIN_LIGHT.INTENSITY
+  );
+  mainLight.position.set(
+    CONSTANTS.MAIN_LIGHT.POSITION.X,
+    CONSTANTS.MAIN_LIGHT.POSITION.Y,
+    CONSTANTS.MAIN_LIGHT.POSITION.Z
+  );
+  scene.add(mainLight);
+
+  const fillLight1 = new THREE.DirectionalLight(
+    0xffffff,
+    CONSTANTS.FILL_LIGHT_1.INTENSITY
+  );
+  fillLight1.position.set(
+    CONSTANTS.FILL_LIGHT_1.POSITION.X,
+    CONSTANTS.FILL_LIGHT_1.POSITION.Y,
+    CONSTANTS.FILL_LIGHT_1.POSITION.Z
+  );
+  scene.add(fillLight1);
+
+  const fillLight2 = new THREE.DirectionalLight(
+    0xffffff,
+    CONSTANTS.FILL_LIGHT_2.INTENSITY
+  );
+  fillLight2.position.set(
+    CONSTANTS.FILL_LIGHT_2.POSITION.X,
+    CONSTANTS.FILL_LIGHT_2.POSITION.Y,
+    CONSTANTS.FILL_LIGHT_2.POSITION.Z
+  );
+  scene.add(fillLight2);
 }
 
 /**
